00627aaacd229498d667dfa714d6813a31297b00
Michele Locati <michele@locati.it>
2015-02-18 14:23:45 +0100

    Add composer dependencies

---
+++ helpers/composer.php
@@ -0,0 +1,17 @@
+<?php defined('C5_EXECUTE') or die('Access denied.');
+
+class ComposerHelper
+{
+    public function loadAutoloaders()
+    {
+        if (!class_exists('\Gettext\Translations', true)) {
+            Loader::library('3rdparty/gettext/gettext/src/autoloader', 'localizer');
+        }
+        if (!class_exists('\C5TL\Languages\Language', true)) {
+            Loader::library('3rdparty/gettext/languages/src/autoloader', 'localizer');
+        }
+        if (!class_exists('\C5TL\Parser', true)) {
+            Loader::library('3rdparty/mlocati/concrete5-translation-library/src/autoloader', 'localizer');
+        }
+    }
+}
